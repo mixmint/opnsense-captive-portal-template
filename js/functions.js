e0b4e62305efea2b1e9222b47aa1e90758054881
Mirosław Majka <mix@proask.pl>
2025-06-07 11:46:31 +0200

    New translation - German

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 /**
- * @version 2.1.1
+ * @version 2.1.2
  * @package Multilanguage Captive Portal Template for OPNsense
  * @author Mirosław Majka (mix@proask.pl)
  * @copyright (C) 2025 Mirosław Majka <mix@proask.pl>
